cca506bf501f6fe3f106f80ed1a78f7e3b962f0b
Fernando de Souza <fernando_dsouza@icloud.com>
2025-09-09 09:09:31 -0300

    feat: agora o mês do componente Banner muda de forma dinâmica

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,14 @@
 export default function Banner() {
+  const month = new Date().toLocaleString("pt-Br", { month: "long" })
+  const capitalizedMonth = month.charAt(0).toUpperCase() + month.slice(1)
+
   return (
     <div className="relative bg-neutral-200/35 rounded-b-full p-2 h-60 flex flex-col items-center justify-center shadow-lg">
       <h1 className="text-6xl font-bold text-neutral-950 text-right leading-12 relative">
         agenda <br /> aberta
       </h1>
       <span className="absolute text-white text-3xl font-sans
-       italic ml-18 mb-8 flex items-center shadow-2xl"><span className="text-5xl">A</span>gosto</span>
+       italic ml-18 mb-8 flex items-center shadow-2xl">{capitalizedMonth}</span>
       <span className="text-lg p-2">Nova Friburgo</span>
     </div>
   )
